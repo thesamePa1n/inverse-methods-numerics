a9633c84158cc41c74d2e0d672afcbe4383fca4e
Vasya <VasyaPain2004@gmail.com>
2026-01-29 14:47:30 +0900

    laba1

---
+++ laba1/1-3.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N = 100
+x = np.linspace(0, 1, N+1)
+h = 1 / N
+
+A = np.zeros((N, N))
+for i in range(1, N-1):
+    A[i, i-1] = -1
+    A[i, i] = 2
+    A[i, i+1] = -1
+    
+A[0, 0] = 2
+A[0, 1] = -1
+A[-1, -2] = -2
+A[-1, -1] = 2 + 2*h
+
+A /= h**2
+
+f = 1.0
+b = np.ones(N) * f
+
+u = np.zeros_like(x)
+u0 = 0
+u[0] = u0
+
+b[0] += u[0] / h**2
+b[-1] = f
+
+def solver(A, b):
+    return np.linalg.solve(A, b)
+
+u_ = solver(A, b)
+
+u[1:] = u_
+
+fig, ax = plt.subplots()
+plt.plot(x, u)
+plt.show()
